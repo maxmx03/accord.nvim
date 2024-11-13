a8ef0440b4923223984c01bcb49e1b0b9de1f384
Max Miliano <milianordelcanto@gmail.com>
2024-11-13 14:18:05 -0300

    feat: accordicon linked to keyword

--- lua/accord/highlights.lua
+++ lua/accord/highlights.lua
@@ -1,16 +1,11 @@
 local api = vim.api
-local colors = {
-  dark = '#f5eccf',
-  light = '#a9a083',
-}
-local background = vim.o.background
 local hl = api.nvim_set_hl
 local ns_id = 0
 local text = api.nvim_get_hl(ns_id, { name = 'AccordText' })
 local icon = api.nvim_get_hl(ns_id, { name = 'AccordIcon' })
 
 if vim.tbl_isempty(icon) then
-  hl(ns_id, 'AccordIcon', { fg = colors[background] })
+  hl(ns_id, 'AccordIcon', { link = 'Keyword' })
 end
 if vim.tbl_isempty(text) then
   hl(ns_id, 'AccordText', { link = 'Comment' })
